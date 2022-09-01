aa7986b1b765a38a5180edfc988f7f35d733fb2d
Minart <mino.mnz@gmail.com>
2022-09-01 11:32:57 +0200

    Use antfu configuration netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,10 +8,24 @@ export default defineNuxtConfig({
     '@/assets/styles/arimo.scss',
     '@/assets/styles/raleway.scss'
   ],
-  buildModules: [
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false
+  },
+  modules: [
     '@nuxt-hero-icons/outline/nuxt',
     '@nuxt-hero-icons/solid/nuxt'
   ],
+  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
+  hooks: {
+    'vite:extendConfig': function (config: any, { isServer }: any) {
+      if (isServer) {
+        // Workaround for netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    }
+  },
   vite: {
     plugins: [
       eslintPlugin()
